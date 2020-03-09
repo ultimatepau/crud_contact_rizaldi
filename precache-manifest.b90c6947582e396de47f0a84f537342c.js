30db96302f3bfc14b1cf07e5a61131af1d1ee242
ultimatepau <mrizaldi2@gmail.com>
2020-03-09 16:15:41 +0700

    Updates

---
+++ precache-manifest.b90c6947582e396de47f0a84f537342c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "048c805aa7f6b1ff68814d8d64a1f4e1",
+    "url": "/crud_contact_rizaldi/index.html"
+  },
+  {
+    "revision": "ea4e2cc1bc781902e86d",
+    "url": "/crud_contact_rizaldi/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b76092452c97cea12401",
+    "url": "/crud_contact_rizaldi/static/js/2.3284b672.chunk.js"
+  },
+  {
+    "revision": "39ef21fe171d78559bfc4c7798303c06",
+    "url": "/crud_contact_rizaldi/static/js/2.3284b672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea4e2cc1bc781902e86d",
+    "url": "/crud_contact_rizaldi/static/js/main.d94a458f.chunk.js"
+  },
+  {
+    "revision": "36db8e75db59b4a56b97",
+    "url": "/crud_contact_rizaldi/static/js/runtime-main.7734ccec.js"
+  }
+]);
